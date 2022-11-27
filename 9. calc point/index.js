4ff4cae2e69b092617c6625c979e1999eb211e27
unknown <cjc2929@naver.com>
2022-11-27 14:04:58 +0900

    9

---
+++ 9. calc point/index.js
@@ -0,0 +1,48 @@
+const addPoint = document.getElementById("addPoint");
+const myPoint = document.getElementById("myPoint");
+const resultPoint = document.getElementById("resultPoint");
+const addBtn = document.getElementById("addBtn");
+const deductPoint = document.getElementById("deductPoint");
+const deductBtn = document.getElementById("deductBtn");
+
+const payAmount = document.getElementById("payAmount");
+const payAmount5per = document.getElementById("payAmount5per");
+const redeemablePoint = document.getElementById("redeemablePoint");
+const finalPayment = document.getElementById("finalPayment");
+
+function addPointSelect(){
+  const sel = document.getElementById("pointSelect");
+  const val = sel.options[sel.selectedIndex].value;
+  const addPoint = document.getElementById("addPoint");
+  const pointList = {
+    signup: 50000,
+    appointmentReview : 30000,
+    academyReviewFirst: 20000,
+    academyReview: 20000,
+    referral: 10000
+  }
+  addPoint.value = pointList[val];
+}
+
+function myPointValue(){
+  resultPoint.value = myPoint.value;
+}
+addBtn.addEventListener("click", function(){
+  resultPoint.value = Number(resultPoint.value) + Number(addPoint.value)
+})
+
+deductBtn.addEventListener("click", function(){
+  if(resultPoint.value === '0') {
+    return false
+  } else {
+    resultPoint.value = Number(resultPoint.value) - Number(deductPoint.value)
+  }
+})
+
+function calcPayAmount(){
+  let calcPayAmount5per = payAmount.value * 0.05;
+  let calcRedeemablePoint = Math.floor(calcPayAmount5per * 0.0001) * 10000;
+  payAmount5per.value = calcPayAmount5per;
+  redeemablePoint.value = calcRedeemablePoint;
+  finalPayment.value = payAmount.value - calcRedeemablePoint;
+}
